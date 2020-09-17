aba07a673ffdfdff778c3bab822efde9c3c06c50
ScarletRyu <jpintool17dw@ikzubirimanteo.com>
2020-09-17 09:48:49 +0200

    added js and accordion

---
+++ Merch.php
@@ -0,0 +1,78 @@
+<div id="merchandaising">
+  <div class="row">
+    <div class="column_merchandaising">
+      <table>
+        <tr>
+          <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img  class="img_producto" src=""></a>
+           <p class="desc_merchandaising">Taza</p>
+           <p class="precio_merchandaising">10 €</p>
+         </div>
+          </td>
+           <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Carcasa Movil</p>
+           <p class="precio_merchandaising">12 €</p>
+         </div>
+          </td>
+          <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Lienzo 5 Paneles</p>
+           <p class="precio_merchandaising">20 €</p>
+         </div>
+          </td>
+          </tr>
+          <tr>
+           <td>
+             <div class="producto">
+               <a href="https://www.google.com"><img class="img_producto" src=""></a>
+             <p class="desc_merchandaising">Taza</p>
+             <p class="precio_merchandaising">15 €</p>
+         </div>
+          </td>
+           <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Carcasa</p>
+           <p class="precio_merchandaising">20 €</p>
+         </div>
+          </td>
+            <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Poster</p>
+           <p class="precio_merchandaising">20 €</p>
+         </div>
+          </td>
+       </tr>
+       <tr>
+          <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Poster</p>
+           <p class="precio_merchandaising">20 €</p>
+         </div>
+          </td>
+            <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Gorra</p>
+           <p class="precio_merchandaising">30 €</p>
+         </div>
+          </td>
+           <td>
+         <div class="producto">
+           <a href="https://www.google.com"><img class="img_producto" src=""></a>
+            <p class="desc_merchandaising">Camiseta</p>
+           <p class="precio_merchandaising">10 €</p>
+         </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+
