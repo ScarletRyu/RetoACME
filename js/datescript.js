f594fc6a2fdcf6e78535e63050beb1e398554c3b
ScarletRyu <jpintool17dw@ikzubirimanteo.com>
2020-09-17 12:41:55 +0200

    Added time and date

---
+++ js/datescript.js
@@ -0,0 +1,15 @@
+//-----------------DATE & TIME--------------------//
+function doDate()
+{
+    var str = "";
+
+    var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+    var now = new Date();
+
+    str += days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+    document.getElementById("hora_fecha").innerHTML = str;
+}
+
+setInterval(doDate, 1000);
