a8ca1dd9277ce540b3a58c667366c022e45c53d1
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-09-18 08:55:24 +0200

    changed date format

--- js/datescript.js
+++ js/datescript.js
@@ -2,13 +2,9 @@
 function doDate()
 {
     var str = "";
-
-    var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
     var now = new Date();
-
-    str += days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+    var mes = now.getMonth()+1;
+    str += now.getDay() + "/" + now.getDate() + "/" + mes+ "/" + now.getFullYear() + "<br> " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
     document.getElementById("hora_fecha").innerHTML = str;
 }
 
